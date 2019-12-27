4f09625959675dff38b241a4a3a09595c143e731
Gamer_Lee <756092470@qq.com>
2019-12-27 21:35:31 +0800

    add rectangle for face

--- face.py
+++ face.py
@@ -3,10 +3,24 @@ import face_recognition
 from PIL import Image
 import numpy as np
 import cv2 as cv
+import os
 
-source_name = "1.JPG"  # 第一张照片（用于创建所有同学的面部信息库）
-new = ["2.JPG", "3.JPG", ]  # 后续会添加的照片（用于补充面部信息库）
-sign_image = 'test.JPG'  # 用于签到的照片
+path = os.path.abspath('.')
+path += '\\student\\'
+is_exists = os.path.exists(path)
+if not is_exists:
+    os.mkdir(path)
+# is_file_exists = os.path.exists(path+'studentList.txt')
+# if not is_file_exists:
+#     f = open(path + 'studentList.txt', 'w+')
+# else:
+#     f = open(path+'studentList.txt', 'a+')
+# a = list(f)
+
+source_name = "person1.JPG"  # 第一张照片（用于创建所有同学的面部信息库）
+new = []  # 后续会添加的照片（用于补充面部信息库）
+sign_image = '2222.JPG'  # 用于签到的照片
+
 
 image = face_recognition.load_image_file(source_name)
 
@@ -23,7 +37,8 @@ def build_set(new, image):
         # 开始截取
         region = img.crop((face_locations[i][3], face_locations[i][0], face_locations[i][1], face_locations[i][2]))
         # 保存图片
-        region.save('Student' + str(i) + '.PNG')
+        region.save(path + 'Student' + str(i) + '-test.PNG')
+        # f.write('Student' + str(num - 1) + '-test.PNG'+'\n')
     # 查找面部编码
     list_of_face_encodings = face_recognition.face_encodings(image)
     for new_image in new:
@@ -41,7 +56,8 @@ def build_set(new, image):
                 img2 = Image.open(new_image)
                 region2 = img2.crop((locations2[i][3], locations2[i][0], locations2[i][1], locations2[i][2]))
                 # 保存图片
-                region2.save('Student' + str(num - 1) + '.PNG')
+                region2.save(path + 'Student' + str(num - 1) + '.PNG')
+                # f.write('Student' + str(num - 1) + '.PNG'+'\n')
     return num, list_of_face_encodings
 
 
@@ -63,14 +79,15 @@ def sign_in(sign_image, list_of_face_encodings, total_num):
             # 保存图片
             cv.rectangle(im, (sign_locations[p][3], sign_locations[p][0]), (sign_locations[p][1], sign_locations[p][2]),
                          (0, 255, 0), 3)
-            p_name1 = '新同学'
+            p_name1 = 'new student'
             if (sign_locations[p][0] > 10):
                 cv.putText(im, p_name1, (sign_locations[p][3], sign_locations[p][0] - 6), cv.FONT_HERSHEY_COMPLEX_SMALL,
                            0.8, (0, 255, 0))
             else:
                 cv.putText(im, p_name1, (sign_locations[p][3], sign_locations[p][0] + 15),
                            cv.FONT_HERSHEY_COMPLEX_SMALL, 0.8, (0, 255, 0))
-            region3.save('Student' + str(num_new) + 'new.PNG')
+            region3.save(path + 'Student' + str(num_new) + '.PNG')
+            # f.write('Student' + str(num_new) + '.PNG'+'\n')
             num_new = num_new + 1
 
         else:  # 如果此人出现过，找出与此人面部编码欧氏距离最近的一个签到
@@ -121,7 +138,8 @@ def print_absent(absent_list):
             img_list = []
             for i in range(rowNum):
                 if index in range(len(absent_list)):
-                    img = cv.imread('Student' + str(absent_list[index]) + '.PNG')
+                    img = cv.imread(path + 'Student' + str(absent_list[index]) + '.PNG')
+                    # img = cv.imread(path + a[i])
                     new_img = cv.resize(img, (100, 100))
                     img_list.append(new_img)
                     index += 1
@@ -133,11 +151,16 @@ def print_absent(absent_list):
             final_img = cv.vconcat(row_img)
         else:
             final_img = row_img[0]
+        cv.namedWindow("Who is absent?", 0)
         cv.imshow("Who is absent?", final_img)
         cv.waitKey(0)
         cv.destroyAllWindows()
 
 
 numbers, encodings = build_set(new, image)
+
 absent_list = sign_in(sign_image, encodings, numbers)
+
 print_absent(absent_list)
+
+# f.close()
